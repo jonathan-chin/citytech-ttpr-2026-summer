fe41d134943da04bd8674e9d0fa7965e98bc2456
Jon Chin <jon@sharebyteaching.org>
2026-07-01 11:05:47 -0400

    provided example of tanstack query and api call / error handling

--- demos/pokemon-lookup/src/main.tsx
+++ demos/pokemon-lookup/src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
 import App from './App';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- demos/pokemon-lookup/src/pages/Home.tsx
+++ demos/pokemon-lookup/src/pages/Home.tsx
@@ -1,25 +1,54 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+// @ts-nocheck
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+import { useQuery } from "@tanstack/react-query";
+import {
+  IonButton,
+  IonInput
+} from '@ionic/react';
+import {
+  useForm,
+  Controller,
+} from 'react-hook-form';
+import {
+  useState
+} from 'react';
+
+const PokemonWeight = ({name}: {name: string}) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["pokemon", name],
+    queryFn: () =>
+      fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((res) => res.json()),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong</p>;
+  return <p>{data.name} weighs {data.weight}</p>;
+}
+
+const Home = () => {
+  const [name, setName] = useState('pikachu');
+  const { control, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    setName(data.name);
+  };
+
+  return <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="name"
+        control={control}
+        render={({ field }) => (
+          <IonInput
+            label="name"
+            fill='outline'
+            value={field.value}
+            onIonChange={(e) => field.onChange(e.detail.value)}
+          />
+        )}
+      />
+      <PokemonWeight name={name} />
+      </form>
+  </>;
+}
 
 export default Home;
