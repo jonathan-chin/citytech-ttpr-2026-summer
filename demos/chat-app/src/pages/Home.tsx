f00711df4579e7ce132e9ebdbe9bae9365a6ec60
Jon Chin <jon@sharebyteaching.org>
2026-07-01 10:24:36 -0400

    added example of chat app using RHF and reset

--- demos/chat-app/src/pages/Home.tsx
+++ demos/chat-app/src/pages/Home.tsx
@@ -1,25 +1,60 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+// @ts-nocheck
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+import {
+  IonButton,
+  IonInput
+} from '@ionic/react';
+import {
+  useForm,
+  Controller,
+} from 'react-hook-form';
+
+const Home = () => {
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      name: '',
+      message: '',
+    }
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+    reset({
+      name: data.name,
+      message: ''
+    });
+  };
+
+  return <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="name"
+        control={control}
+        render={({ field }) => (
+          <IonInput
+            label="name"
+            fill='outline'
+            value={field.value}
+            onIonChange={(e) => field.onChange(e.detail.value)}
+          />
+        )}
+      />
+      <Controller
+        name="message"
+        control={control}
+        render={({ field }) => (
+          <IonInput
+            label="message"
+            fill='outline'
+            value={field.value}
+            onIonChange={(e) => field.onChange(e.detail.value)}
+          />
+        )}
+      />
+      <IonButton type='submit'>
+        Send
+      </IonButton>
+    </form>
+  </>
 };
 
 export default Home;
